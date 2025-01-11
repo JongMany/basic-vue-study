42e59878655b920eb8f9e50f92ef55808290e7af
JongMany <blackberry1114@naver.com>
2025-01-11 22:03:04 +0900

    v-model로 input 연동

---
+++ vue3-form/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <form action="">
+    <div>
+      <label for="id">아이디</label>
+      <input type="text" id="id" v-model="username" />
+    </div>
+    <div>
+      <label for="password">비밀번호</label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+};
+</script>
+
+<style scoped></style>
